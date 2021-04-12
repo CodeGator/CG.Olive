bc9d3331a1cdeafaba0cbfd929582448f58c2fac
martycook <martinc@valornet.com>
2021-04-12 13:09:59 -0500

    I edited the docs

--- docfx/index.md
+++ docfx/index.md
@@ -1,3 +1,5 @@
+<img src="~/images/codegator-334x158.png" />
+
 # Introduction
 
 CG.Olive is a micro-service for managing application configurations.
--- src/Web/CG.Olive.Web/Startup.cs
+++ src/Web/CG.Olive.Web/Startup.cs
@@ -1,6 +1,5 @@
 using CG.Validations;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
